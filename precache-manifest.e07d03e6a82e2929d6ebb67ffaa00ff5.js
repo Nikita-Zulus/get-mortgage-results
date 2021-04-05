db1a6565453c39fb163db5721487759a40c1f0f8
Никита <nikita-zulus@mail.ru>
2021-04-05 23:54:32 +0300

    Updates

---
+++ precache-manifest.e07d03e6a82e2929d6ebb67ffaa00ff5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9855894620b4ab261785e319ba31e063",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e616188b239d2f34e0d",
+    "url": "/static/css/main.f626d25b.chunk.css"
+  },
+  {
+    "revision": "7551fd06768998aeb026",
+    "url": "/static/js/2.787d55d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.787d55d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e616188b239d2f34e0d",
+    "url": "/static/js/main.4dc19e5e.chunk.js"
+  },
+  {
+    "revision": "c95b99eccb4a91f30f14",
+    "url": "/static/js/runtime-main.887ccb90.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "0be92c3b0b9911fbd7585d3d5c19742b",
+    "url": "/static/media/arrow-down.0be92c3b.svg"
+  },
+  {
+    "revision": "cc7401444b817b0a9cdcbd20814bc8e1",
+    "url": "/static/media/bank-bjf.cc740144.svg"
+  },
+  {
+    "revision": "0f2042187e6852d3f01ebb73ef6bc448",
+    "url": "/static/media/bank-vtb.0f204218.svg"
+  },
+  {
+    "revision": "02254f8eec7fb4d6c9f9742aaf9ae443",
+    "url": "/static/media/burger-menu.02254f8e.svg"
+  },
+  {
+    "revision": "99609c0f50dfadcfc8b2f2d9d2a1b345",
+    "url": "/static/media/calendar.99609c0f.svg"
+  },
+  {
+    "revision": "49164afd597b880d6f1c2526595be59a",
+    "url": "/static/media/dif-pay.49164afd.svg"
+  },
+  {
+    "revision": "759fc2fb5c898642b3c535fd5edb98d6",
+    "url": "/static/media/gazprom.759fc2fb.svg"
+  },
+  {
+    "revision": "7247fb05eef2a377fdc002ff567ad53a",
+    "url": "/static/media/gerb.7247fb05.svg"
+  },
+  {
+    "revision": "3e9eaa204e3c5ff20e5f458d93c08bed",
+    "url": "/static/media/icon-heart.3e9eaa20.svg"
+  },
+  {
+    "revision": "a0c6c248f0fee1996f85631b5e048a96",
+    "url": "/static/media/logo-cian.a0c6c248.svg"
+  },
+  {
+    "revision": "432dd9add3fd53db7ceb2aa62932d631",
+    "url": "/static/media/online-process.432dd9ad.svg"
+  },
+  {
+    "revision": "c65ad513b6889af5b6458895753df7db",
+    "url": "/static/media/phone.c65ad513.svg"
+  },
+  {
+    "revision": "62884726f8cc756129229e9bd4b8d22f",
+    "url": "/static/media/psb.62884726.svg"
+  },
+  {
+    "revision": "e3fb2ad361dc86d951610c5500a30b88",
+    "url": "/static/media/raifaizen.e3fb2ad3.svg"
+  },
+  {
+    "revision": "b04198b94e6d1fec168762b92039b3dd",
+    "url": "/static/media/settings.b04198b9.svg"
+  },
+  {
+    "revision": "603ac1eea9fd1318cfad30819b22c749",
+    "url": "/static/media/sovcombank.603ac1ee.svg"
+  }
+]);
